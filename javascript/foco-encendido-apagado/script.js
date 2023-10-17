431643e20c5324129cd2e016fb254335c7b58164
VictorCun <vmchero@gmail.com>
2023-10-16 19:55:06 -0600

    arreglo-js-foco

--- javascript/foco-encendido-apagado/script.js
+++ javascript/foco-encendido-apagado/script.js
@@ -1,15 +1,15 @@
 const $cambiarInterruptor = document.getElementById("cambiarInterruptor"),
-      $cambiarFoco = document.getElementById("cambiarFoco"),
-      $swith = document.querySelector(".switch");
+    $cambiarFoco = document.getElementById("cambiarFoco"),
+    $swith = document.querySelector(".switch");
 
-function cambiarImagenes(){
-    if($cambiarFoco.src.match("./images/encendido-1.png") && $cambiarInterruptor.src.match("./images/on-or.png")){
+function cambiarImagenes() {
+    if ($cambiarFoco.src.match("./images/encendido-1.png") && $cambiarInterruptor.src.match("./images/on-or.png")) {
         $cambiarFoco.src = "./images/apagado-1.png";
-        $cambiarInterruptor.src =  "../images/off-or.png";
-    }else{
+        $cambiarInterruptor.src = "./images/off-or.png";
+    } else {
         $cambiarFoco.src = "./images/encendido-1.png";
         $cambiarInterruptor.src = "./images/on-or.png";
     }
 }
 
-$swith.addEventListener("click",cambiarImagenes)
+$swith.addEventListener("click", cambiarImagenes)
